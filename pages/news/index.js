6beafc13ebed6f45e4b897de8998926756448bb6
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-08-27 16:46:04 +0200

    Create news page with getServerSideProps
    - getServerSideProps

---
+++ pages/news/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const NewsArticleList = ({articles}) => {
+  return (
+    <>
+        <h1>list of News Articles</h1>
+        {articles.map(article => {
+            return (
+                <div key={article.id}>
+                    <h2>{article.id} {article.title} | {article.category}</h2>
+                </div>
+            )
+        })}
+    </>
+  )
+}
+
+export default NewsArticleList
+
+
+export const getServerSideProps = async() => {
+    const response = await fetch('http://localhost:4000/news')
+    const data = await response.json()
+    return {
+        props: {
+            articles: data
+            }
+            }
+    
+}
