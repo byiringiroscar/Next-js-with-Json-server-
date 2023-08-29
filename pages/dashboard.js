f5e5126733970fbd2f0dbf53f64b96074852d5b4
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-08-29 11:48:53 +0200

    Client side data fetching

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,7 +1,29 @@
-const dashboard = () => {
+import { useState, useEffect } from "react"
+
+const Dashboard = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [dashboardData, setDashboardData] = useState(null)
+  useEffect(() => {
+    const fetchDashboardData = async () => {
+      const response = await fetch('http://localhost:4000/dashboard')
+      const data = await response.json()
+      setDashboardData(data)
+      setIsLoading(false)
+    }
+    fetchDashboardData()
+  }, [])
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
   return (
-    <div>dashboard</div>
+    <div>
+      <h2>dashboard</h2>
+      <h2>Posts - {dashboardData.posts}</h2>
+      <h2>likes - {dashboardData.likes}</h2>
+      <h2>Followers - {dashboardData.followers}</h2>
+      <h2>Following - {dashboardData.following}</h2>
+    </div>
   )
 }
 
-export default dashboard
+export default Dashboard
